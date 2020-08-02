27552876b9bfae2d7abf640316c7b1d204a2b526
Rylee Bers <ryleebers@hotmail.com>
2020-08-02 14:19:59 -0700

    listen class now doesn't use commandblock object

--- file_io/listen.py
+++ file_io/listen.py
@@ -1,228 +1,74 @@
 """
     Author: Rylee Bers
-    Last modified: 7/19/2020
+    Last modified: 8/2/2020
 
     This file contains the listen class. It's purpose is to listen for a command from the user,
-    then convert the command in a CommandBlock object
+    then pass back to codex functions
 """
 
-#need to edit later
-from gtts import gTTS
-import speech_recognition as sr
-import os
-import re
-import smtplib
-import sphinx
-import smtplib
-import pocketsphinx
-
-
-
-
-#In the future, I think these two classes should take into account the user wanting 
-#to add non variable or function things such as "|, &, ==, ect..."
-
-
+import copy
 
 class listen(object):
-    def __init__(self):
-        #directory to find potential parameters for commandBlock
+    """
+    Initialization of listen class
 
-        #dictionaries are used so that multiple phrases could point to the same meaning
-        #dictionaries are used when there is a predefined list of options the user can choose from
-
-        #a list is used when the commandBlock parameter is made up by the user and not from a predefined list
-
-
-        #directory of user actions
-        #most keys associated with a codex function
-        self.actiondict = {"add function"         : "add_to_func_body",       #jarvis I'm not familiar with your codex functions so feel free to change the values associated with the keys
-                           "add new function"     : "add_func",               #TODO - figure out how to pull out "declaration" or "definition" parameter
-                           "add variable"         : "add_to_func_body",       #TODO - add add_include to the action list and add_file
-                           "add new variable"     : "add_to_func_body",
-                           "add"                  : "add_to_func_body",
-                           "remove"               : "delete",
-                           "delete"               : "delete",
-                           "get rid of"           : "delete",  #change to whatever codex function deletes
-                           "take away"            : "delete",
-                           "change"               : "modify",
-                           "modify"               : "modify",  #change to whatever codex function modifies
-                           "adjust"               : "modify",
-                           "undo"                 : "undo",
-                           "exit"                 : "exit"
-                           }
+    @instance wordbank the list of words that can be converted to commands
+    """
+    def __init__(self, wordbank):
+        self.wordbank = ['add', 'modify', 'delete', 'undo']
 
 
-        #directory of varibale types                   
-        self.typedict = {"integer"                : "int",
-                         "int"                    : "int",
-                         "double"                 : "double",
-                         "character"              : "char",
-                         "char"                   : "char",
-                         "string"                 : "string",
-                         "unsigned integer"       : "unsigned int",
-                         "unsigned int"           : "unsigned int"
-                         #more to come.... 
-                        }
 
 
-        #directory of existing c functions
-        self.funcdict = {"for loop"               : "for",
-                         "for"                    : "for",
-                         "while loop"             : "while",
-                         "while"                  : "while",
-                         #more to come....
-                        }
-        
+    """
+    Autofills a word as user is typing by process of elimination
+    Future use might involve giving the user the option to click the word instead of have it autofill
 
-              
-        #list with phrases that indicate the following number, string, char, ect. is a value to a variable
-        self.valuelist = ['equals', 'with value of', 'set value to']
+    @instance letters the substring of the word the user has typed so far
+    """
+    def autofill(self, letters):
+        potentialwords = self.wordbank.copy()
 
+        #Check potential word letter by letter. If there is any irregularity remove the word from potential
+        #list and move on to next word
+        for word in potentialwords:
+            for c in range(len(letters)):
+                if letters[c] != word[c]:
+                    del word
+                    break
 
-        #list with phrases that indicate the following number represents what line to modify
-        self.linelist = ['line', 'on', 'at']
+        #return word if there is only one choice left
+        if len(potentialwords) == 1:
+            return potentialwords[0]
+        else:
+            return -1
+
 
     
-        #list with phrases that indicate the following word is the name of a variable 
-        #I might add some more flexability in the future so that the user can give a variable
-        #a name without using a keyword
-        self.namelist = ['named', 'called']
+    """
+    Adds new word to wordbank list. 
 
-        #self.columnlist = []
-
+    @instand neword the word to add to list
+    """
+    def add_word(self, newword):
+        self.wordbank.insert(newword)
 
 
 
 
     """
-        When the user creates a new function, it can be added to the directory of existing c functions
-        Might delete later
+    Helper function for function that will autocorrect if a word is a certain percentage match of a word from wordbank
 
-        @instance function the function the user created
-        @instance funcname the name of the function that will be linked with the function (the key in the directory)
+    @instance correct number of characters that match word
+    @instance numletters the total number of characters in the word that is being analyzed
     """
-    def addToFuncdict(self, function, funcname):
-        self.funcdict[funcname] = function
+    def percentage(self, correct, numletters):
+        pass
+
+    
 
 
 
-
-
-    """ 
-        The end goal for this function in the future is that it will be able to speak to the user
-        This was taken directly from desktopAssistant
-
-        @instance audio what the computer will speak to the user
-    """
-    def talkToUser(self, audio):
-        "speaks audio passed as argument"
-
-        print(audio)
-        for line in audio.splitlines():
-            os.system(audio)
-
-        #  use the system's inbuilt say command instead of mpg123
-        #  text_to_speech = gTTS(text=audio, lang='en')
-        #  text_to_speech.save('audio.mp3')
-        #  os.system('mpg123 audio.mp3')
-
-
-
-
-
-
-    """ 
-        Function that converts audio into a single string of the user's command
-        command is then pushed to parseCommand and the interpreted command is returned
-    """
-    def listenToUser(self):
-        #listening to the user
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening")     # it might be annoying hearing "listening" a million times a day so just print it
-            audio = r.listen(source)
-        command = r.recognize_sphinx(audio)
-
-        try:
-            # recognize speech using Sphinx
-            # benefit: Sphinx does not require internet connection
-            # drawback: Sphinx is not consistently accurate, and appears to not listen while interpretting audio 
-            print("We heard: " + command)
-        except sr.UnknownValueError:
-            talkToUser("Audio was misunderstood. Please repeat.")
-        except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))
-
-        return parseCommand(command) 
-
-
-
-
-
-    """ 
-        Function that converts a single string to a CommandBlock object
-        helper function for listenToUser()
-
-        @instance command the string of the user's command to be parsed into a CommandBlock object
-    """
-    def parseCommand(self, command):
-        #initialize commandBlock object
-        parsedCommand = commandBlock()
-
-        #extract the needed information from the command
-        for key in self.actiondict.keys():
-            if key in command:
-                parsedCommand(action = self.actiondict[key])
-
-        for key in self.typedict.keys():
-            if key in command:
-                parsedCommand(type = self.typedict[key])
-
-        for key in self.funcdict:
-            if key in command:
-                parsedCommand(func_name = self.funcdict[key])
-
-        for key in self.valuelist:
-            if key in command:
-                parsedCommand(value = extractWord(command, key))
-
-        for key in self.linelist:
-            if key in command:
-                parsedCommand(line = extractWord(command, key))
-
-        for key in self.namelist:
-            if key in command:
-                parsedCommand(name = extractWord(command, key))
-
-
-
-        #return commandBlock object to be used by Jarvis's code
-        return parsedCommand
-
-
-
-
-
-    """
-        Helper function for parseCommand()
-        uses a keyword in the user's command and extracts the word directly after 
-
-        Example: "Add function to line four"
-                 This is the user's command. The keyword is "line"
-                 We want to extract "four" from this phrase and add it to line parameter of commandBlock object
-
-        @instance command the user's spoken command
-        @instance keyword the word or phrase that precedes the word we need to extract
-    """
-    def extractWord(self, command, keyword):
-        extracted = command.split(keyword)
-        del extracted[0]
-        extracted.split(" ")
-
-        return extracted[0]
-
-
-
+        
 
 
